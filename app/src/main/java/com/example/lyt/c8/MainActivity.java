c6ee02f0c5efd9d6921a89b620c36364d55ef5f8
unknown <>
2016-09-07 14:22:49 +0800

    [上机实践]Activity生命周期 熟悉Activity生命周期，使用Logcat观察各回调函数调用次序。

---
+++ app/src/main/java/com/example/lyt/c8/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.lyt.c8;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG="MyActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG,"onCreate()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        Log.i(TAG,"onDestroy()");
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        Log.i(TAG,"onPause()");
+    }
+
+    @Override
+    protected void onRestart() {
+        // TODO Auto-generated method stub
+        super.onRestart();
+        Log.i(TAG,"onRestart()");
+    }
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        Log.i(TAG,"onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        Log.i(TAG,"onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        // TODO Auto-generated method stub
+        super.onStop();
+        Log.i(TAG,"onStop()");
+    }
+
+
+
+}
